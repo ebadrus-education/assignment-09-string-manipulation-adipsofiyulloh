228973d24927265aa8de4585f20a44922e08b290
adipsofiyulloh <adipsofiyulloh@gmail.com>
2025-11-06 21:37:48 +0700

    Create main.cpp

---
+++ 01_length_size/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string teks = "Halo";
+
+    cout << "Panjang dengan length(): " << teks.length() << endl;
+    cout << "Panjang dengan size(): " << teks.size() << endl;
+
+    return 0;
+}
